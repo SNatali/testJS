9f1cb2e771331d385c3c58ab7842b927f303fead
Natalia Sergieieva <bossovivh@mail.ru>
2016-11-14 01:23:43 +0100

    hm

---
+++ hm2_task2.js
@@ -0,0 +1,28 @@
+
+var allNumbers = [1, 2, 4, 5, 6, 7, 8],
+
+someNumbers = [1, 2, 'привет', 4, 5, 'loftschool', 6, 7, 8],
+
+noNumbers = ['это', 'массив', 'без', 'чисел'];
+
+function isSomeTrue(source, filterFn){
+	for(var i = 0; i < source.length; i++) {
+		if(filterFn(source[i])) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	return isSomeTrue(source, filterFn);
+}
+function isNumber(val) {
+	return typeof val === 'number';
+}
+
+var result = isSomeTrue(allNumbers, isNumber);
+var result2 = isSomeTrue(someNumbers, isNumber);
+var result3 = isSomeTrue(noNumbers, isNumber);
+	console.log(result);
+	console.log(result2);
+	console.log(result3);
+
